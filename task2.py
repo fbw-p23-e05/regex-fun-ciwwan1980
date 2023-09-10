db01a782eb8dda2a094bac10b0d2cda664b0adb5
ciwwan1980 <ajilqado@gmail.com>
2023-09-10 14:46:41 +0200

    updated

--- task2.py
+++ task2.py
@@ -1,2 +1,16 @@
 
 # 2. Write a Python program that matches a string that has an a followed by zero or more b's.
+
+import re
+
+input_string = "abbbbb abb ab a"
+
+pattern = 'ab*' # it matches a and zero or more b
+pattern = 'ab+' # it matches a and at least one b
+pattern = 'ab?' # it matches a and zero or only one b
+match = re.findall(pattern, input_string)
+
+if match:
+    print("Match found:", match)
+else:
+    print("No match found.")
--- task4.py
+++ task4.py
@@ -1,13 +1,28 @@
 # 4. Write a Python program that matches a string that has an a followed by zero or one 'b'.
+# import re
+
+# input_string = "ab"
+
+# pattern = '^ab?$'
+
+# match = re.search(pattern, input_string)
+
+# if match:
+#     print("Match found:", match)
+# else:
+#     print("No match found.")
+
 import re
 
-input_string = "ab"
+# Define the regular expression
+regex = r'^ab?$'
 
-pattern = '^ab?$'
+# Test strings
+test_strings = ["ab", "a", "abb", "abc", "cab", "ac", "b", "abbbbb"]
 
-match = re.search(pattern, input_string)
-
-if match:
-    print("Match found:", match)
-else:
-    print("No match found.")
+# Check if each string matches the regex pattern
+for test_string in test_strings:
+    if re.match(regex, test_string):
+        print(f'"{test_string}" matches: Yes')
+    else:
+        print(f'"{test_string}" matches: No')
